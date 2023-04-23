aa7c62d233a3af8a1c981fd070d3f8c8f009069c
MarceloMosl <marcelomosl@icloud.com>
2023-04-23 19:47:34 -0300

    Feat: Get hotels/hotelID commit para entrega

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,6 @@ import 'express-async-errors';
 import express, { Express } from 'express';
 import cors from 'cors';
 
-import { TicketStatus } from '@prisma/client';
-import { createTicket } from '../tests/factories/tickets-factory';
-import { createPayment } from '../tests/factories/payments-factory';
-import { createHotelRooms, createHotelTicketType, cretaeHotel } from '../tests/factories/hotels-factory';
-import { createEnrollmentWithAddress } from '../tests/factories/enrollments-factory';
-import { generateValidToken } from '../tests/helpers';
-import { createUser } from '../tests/factories/users-factory';
-import paymentRepo from './repositories/payment-repository';
 import { loadEnv, connectDb, disconnectDB } from '@/config';
 
 loadEnv();
@@ -30,21 +22,7 @@ const app = express();
 app
   .use(cors())
   .use(express.json())
-  .get('/health', async (_req, res) => {
-    const user = await createUser();
-    const token = await generateValidToken(user);
-    const enrollment = await createEnrollmentWithAddress(user);
-    const ticketType = await createHotelTicketType();
-    const ticket = await createTicket(enrollment.id, ticketType.id, TicketStatus.RESERVED);
-    await createPayment(ticket.id, ticketType.price);
-    await paymentRepo.updateTicket(ticket.id);
-    const hotel = await cretaeHotel();
-    await createHotelRooms(hotel.id);
-
-    console.log(hotel.id);
-    console.log(token);
-    res.send('OK!');
-  })
+  .get('/health', async (_req, res) => res.send('OK!'))
   .use('/users', usersRouter)
   .use('/auth', authenticationRouter)
   .use('/event', eventsRouter)
