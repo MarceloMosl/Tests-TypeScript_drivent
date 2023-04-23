2c74b0c9048afcb24c966901dee788f75b3cbbdc
MarceloMosl <marcelomosl@icloud.com>
2023-04-23 15:44:46 -0300

    Refact: Middleware Get/Hotels para avaliador

--- src/middlewares/hotel-middleware.ts
+++ src/middlewares/hotel-middleware.ts
@@ -6,16 +6,20 @@ import paymentRepo from '@/repositories/payment-repository';
 import hotelRepo from '@/repositories/hotel-repository';
 
 export async function validateHotel(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-  const enrollmentExists = await ticketsRepo.findEnrollmentbyUserId(req.userId);
-  if (!enrollmentExists) return res.sendStatus(httpStatus.NOT_FOUND);
-  const ticketExists = await ticketsRepo.findFirstTicket(enrollmentExists.id);
-  if (!ticketExists) return res.sendStatus(httpStatus.NOT_FOUND);
-  const hotelExists = await hotelRepo.getAllHotels();
-  if (hotelExists.length === 0) return res.sendStatus(httpStatus.NOT_FOUND);
-  const paymentExists = await paymentRepo.getPaymentByTicketId(ticketExists.id);
-  if (!paymentExists) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-  if (!ticketExists.TicketType.includesHotel || ticketExists.TicketType.isRemote)
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+  try {
+    const enrollmentExists = await ticketsRepo.findEnrollmentbyUserId(req.userId);
+    if (!enrollmentExists) return res.sendStatus(httpStatus.NOT_FOUND);
+    const ticketExists = await ticketsRepo.findFirstTicket(enrollmentExists.id);
+    if (!ticketExists) return res.sendStatus(httpStatus.NOT_FOUND);
+    const paymentExists = await paymentRepo.getPaymentByTicketId(ticketExists.id);
+    if (!paymentExists) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    if (ticketExists.TicketType.isRemote) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+    const hotelExists = await hotelRepo.getAllHotels();
+    if (hotelExists.length === 0) return res.sendStatus(httpStatus.NOT_FOUND);
+    if (!ticketExists.TicketType.includesHotel) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
 
-  next();
+    return next();
+  } catch (error) {
+    return res.status(400).send(error);
+  }
 }
