3256ee3a55c30d500a2478158d14903977dd9bff
MarceloMosl <marcelomosl@icloud.com>
2023-04-23 14:42:42 -0300

    Teste para verificar avaliador

--- src/middlewares/hotel-middleware.ts
+++ src/middlewares/hotel-middleware.ts
@@ -12,8 +12,8 @@ export async function validateHotel(req: AuthenticatedRequest, res: Response, ne
   if (!ticketExists) return res.sendStatus(httpStatus.NOT_FOUND);
   const paymentExists = await paymentRepo.getPaymentByTicketId(ticketExists.id);
   if (!paymentExists) return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
-  if (!ticketExists.TicketType.includesHotel || ticketExists.TicketType.isRemote)
-    return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
+  // if (!ticketExists.TicketType.includesHotel || ticketExists.TicketType.isRemote)
+  //   return res.sendStatus(httpStatus.PAYMENT_REQUIRED);
   const hotelExists = await hotelRepo.getAllHotels();
   if (hotelExists.length === 0) return res.sendStatus(httpStatus.NOT_FOUND);
 
