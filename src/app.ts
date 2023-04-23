01604da27eb5d95c43a46884070ad5d3c7377cc1
MarceloMosl <marcelomosl@icloud.com>
2023-04-23 09:55:56 -0300

    Refact get health

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ const app = express();
 app
   .use(cors())
   .use(express.json())
-  .get('/health', async (_req, res) => res.send('OK'))
+  .get('/health', async (_req, res) => res.send('OK!'))
   .use('/users', usersRouter)
   .use('/auth', authenticationRouter)
   .use('/event', eventsRouter)
